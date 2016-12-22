877f20ad94dbb54c3725ad6aa5e168407e6247a2
SAMAKE1 <samakem92@yahoo.fr>
2016-12-22 09:49:45 +0100

    missing files

--- src/main/java/com.brestopencampus.spark/Application.java
+++ src/main/java/com.brestopencampus.spark/Application.java
@@ -43,8 +43,8 @@ public class Application {
 
   private static DB mongo() throws Exception {
     String host = System.getenv("MONGODB_ADDON_HOST");
-    int port = Integer.parseInt(Sytem.getenv("MONGODB_ADDON_PORT")) //27017;Integer.parseInt(System.getenv("MONGODB_DB_PORT"));
-    String dbname =  Sytem.getenv("MONGODB_ADDON_DB")//"HelloSpark";//System.getenv("HelloSpark");
+    int port = Integer.parseInt(Sytem.getenv("MONGODB_ADDON_PORT")); //27017;Integer.parseInt(System.getenv("MONGODB_DB_PORT"));
+    String dbname =  System.getenv("MONGODB_ADDON_DB");//"HelloSpark";//System.getenv("HelloSpark");
     String username = System.getenv("MONGODB_ADDON_USER");
     String password = System.getenv("MONGODB_ADDON_PASSWORD");
     MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
